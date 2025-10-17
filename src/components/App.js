7bd06f8ed491a1300bb8e40b158d5a0ad2f3bb12
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-10-17 16:09:46 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const suggestions = ["Mumbai", "Delhi", "Haryana", "Ayodhaya"]
+  const[text, setText] = useState("")
+  const[xyz, setXyz] = useState([])
+
+  useEffect(() => {
+    if(text == "")return
+    const res = suggestions.filter((item) => {
+      return item.toLowerCase().includes(text.toLowerCase())
+    })
+    // console.log(res)
+    setXyz(res)
+  }, [text])
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input onChange={(e) => {
+          setText(e.target.value)
+        }}/>
+        <ul>
+          {xyz.map((item) => {
+            return <li>{item}</li>
+          })}
+        </ul>
     </div>
   )
 }
